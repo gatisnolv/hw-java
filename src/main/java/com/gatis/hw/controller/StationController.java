7575661ae849f6feeccfcd7f7e64738fda40d02a
Gatis Liepinlauskis <gatis.liepinlauskis@cgi.com>
2024-12-08 23:09:43 +0200

    implement simple json data downloading to top level dir

--- src/main/java/com/gatis/hw/controller/StationController.java
+++ src/main/java/com/gatis/hw/controller/StationController.java
@@ -1,17 +1,26 @@
 package com.gatis.hw.controller;
 
+import com.gatis.hw.service.StationsInfoDownloadService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 @RestController
 @RequestMapping("station")
 public class StationController {
 
+    @Autowired
+    StationsInfoDownloadService service;
+
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public String getAll() {
+    public String getAll() throws IOException, InterruptedException, URISyntaxException {
+        service.downloadStationsInfo();
         return "all stations";
     }
 
