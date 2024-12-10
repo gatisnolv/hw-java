12a7f87c47abd009842368a830ef2e48ef9db240
Gatis Liepinlauskis <gatis.liepinlauskis@cgi.com>
2024-12-10 14:38:23 +0200

    change link to fetch csv instead of json - works to download to top level dir in both docker and locally

--- src/main/java/com/gatis/hw/service/StationsInfoDownloadService.java
+++ src/main/java/com/gatis/hw/service/StationsInfoDownloadService.java
@@ -17,11 +17,11 @@ public class StationsInfoDownloadService {
     public void downloadStationsInfo() throws IOException, InterruptedException, URISyntaxException {
         // TODO add graceful handling of fail
         // TODO add timeout
-        String uri = "https://data.gov.lv/dati/lv/datastore/dump/c32c7afd-0d05-44fd-8b24-1de85b4bf11d?format=json";
+        String uri = "https://data.gov.lv/dati/lv/datastore/dump/c32c7afd-0d05-44fd-8b24-1de85b4bf11d?bom=True";
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(uri)).build();
         // downloads into top level dir
-        HttpResponse<Path> response = client.send(request, HttpResponse.BodyHandlers.ofFile(Paths.get("stations.json")));
+        HttpResponse<Path> response = client.send(request, HttpResponse.BodyHandlers.ofFile(Paths.get("stations.csv")));
         // downloads into ./target/classes/com/gatis/hw/stations.json
 //        HttpResponse<Path> response = client.send(request, HttpResponse.BodyHandlers.ofFile(Path.of(getClass().getResource("").toURI()).resolveSibling("stations.json")));
         // downloads into ./target/classes/com/gatis/hw/service/stations.json
@@ -31,11 +31,11 @@ public class StationsInfoDownloadService {
     public static void get(String uri) throws Exception {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(uri)).build();
-        HttpResponse<Path> response = client.send(request, HttpResponse.BodyHandlers.ofFile(Paths.get("stations.json")));
+        HttpResponse<Path> response = client.send(request, HttpResponse.BodyHandlers.ofFile(Paths.get("stations.csv")));
     }
 
     public static void main(String[] args) throws Exception {
-        get("https://data.gov.lv/dati/lv/datastore/dump/c32c7afd-0d05-44fd-8b24-1de85b4bf11d?format=json");
+        get("https://data.gov.lv/dati/lv/datastore/dump/c32c7afd-0d05-44fd-8b24-1de85b4bf11d?bom=True");
     }
 
 }
