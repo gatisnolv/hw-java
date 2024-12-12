c68b901157a37cd2b63438da2880527c186c05c4
Gatis Liepinlauskis <gatis.liepinlauskis@cgi.com>
2024-12-12 15:36:34 +0200

    use httpclient in a Spring idiomatic way

--- src/main/java/com/gatis/hw/service/StationsDataDownloadService.java
+++ src/main/java/com/gatis/hw/service/StationsDataDownloadService.java
@@ -2,38 +2,37 @@ package com.gatis.hw.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
-import java.io.IOException;
-import java.net.ConnectException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpTimeoutException;
-import java.nio.file.Paths;
-import java.time.Duration;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
 
 @Service
 @Slf4j
 public class StationsDataDownloadService {
 
-    HttpClient client = HttpClient.newHttpClient();
+    private final RestClient restClient;
 
-    public boolean downloadStationsData() throws IOException, InterruptedException {
+    public StationsDataDownloadService(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder.build();
+    }
+
+    public boolean downloadStationsData() {
         String uri = "https://data.gov.lv/dati/lv/datastore/dump/c32c7afd-0d05-44fd-8b24-1de85b4bf11d?bom=True";
-        HttpRequest request = HttpRequest.newBuilder(URI.create(uri)).timeout(Duration.ofMinutes(1)).build();
-        try {
-            HttpResponse<?> response = client.send(request, HttpResponse.BodyHandlers.ofFile(Paths.get("stations.csv")));
-            if (HttpStatusCode.valueOf(response.statusCode()) == HttpStatus.OK) return true;
-            else log.error("Unexpected response code: {}", response.statusCode());
-        } catch (ConnectException e) {
-            log.error("Network unreachable");
-        } catch (HttpTimeoutException e) {
-            log.error("Request timed out");
-        }
-        return false;
+        return Boolean.TRUE.equals(restClient.get().uri(uri).exchange((request, response) -> {
+            if (HttpStatus.OK == response.getStatusCode()) {
+                File stationsFile = new File("stations.csv");
+                Files.copy(response.getBody(), stationsFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                log.info("Updated stations data");
+                return true;
+            } else {
+                log.error("Unexpected response code: {}", response.getStatusCode());
+                return false;
+            }
+        }));
     }
 
 }
