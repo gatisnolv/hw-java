16118c1478d2d834d665749c485d74cebfa2aa47
Gatis Liepinlauskis <gatis.liepinlauskis@cgi.com>
2024-12-12 15:41:18 +0200

    add proper error handling for the reworked download funcionality

--- src/main/java/com/gatis/hw/controller/StationController.java
+++ src/main/java/com/gatis/hw/controller/StationController.java
@@ -1,18 +1,19 @@
 package com.gatis.hw.controller;
 
 import com.gatis.hw.service.StationsDataDownloadService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
+import java.net.ConnectException;
+import java.net.http.HttpTimeoutException;
+import java.util.concurrent.CancellationException;
 
 @RestController
 @RequestMapping("station")
+@Slf4j
 public class StationController {
 
     @Autowired
@@ -26,6 +27,19 @@ public class StationController {
     @GetMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getOne(@PathVariable String id) {
         return "one station";
+    }
+
+    // TODO does the reason string show up in openapi?
+    @ResponseStatus(value = HttpStatus.SERVICE_UNAVAILABLE, reason = "Network unreachable")
+    @ExceptionHandler({ConnectException.class})
+    public void handleConnectException(Exception e) {
+        log.error("Network unreachable");
+    }
+
+    @ResponseStatus(value = HttpStatus.SERVICE_UNAVAILABLE, reason = "Request timed out")
+    @ExceptionHandler({HttpTimeoutException.class, CancellationException.class})
+    public void handleHttpTimeoutException(Exception e) {
+        log.error("Request timed out");
     }
 
 }
